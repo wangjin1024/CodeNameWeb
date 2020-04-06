ca69234ce3677ed55279a74505949dd434ccae41
Wang Jin <jin.wang02@sap.com>
2020-04-06 23:36:18 +0800

    initial commit for chat room with django

---
+++ codename/game/views.py
@@ -0,0 +1,10 @@
+# chat/views.py
+from django.shortcuts import render
+
+def index(request):
+    return render(request, 'game/index.html')
+
+def room(request, room_name):
+    return render(request, 'game/room.html', {
+        'room_name': room_name
+    })
